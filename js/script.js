d2c604fe97107a75cf597d3305ae2c965d3447a8
Bruno Souza <bruno.souza@bhs.com.br>
2020-04-13 17:20:47 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,57 @@
+$(function () {
+    $(".previous").click(function () {
+
+        current_fs = $(this).parent().parent().parent();
+        previous_fs = $(this).parent().parent().parent().prev();
+
+        //show the previous fieldset
+        previous_fs.show();
+
+        //hide the current fieldset with style
+        current_fs.animate({ opacity: 0 }, {
+            step: function (now) {
+                // for making fielset appear animation
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                previous_fs.css({ 'opacity': opacity });
+            },
+            duration: 500
+        });
+
+        scrollToAnchor(".news__section", 300);
+    });
+
+    $(".next").on("click", function () {
+
+        current_fs = $(this).parent().parent().parent();
+        next_fs = $(this).parent().parent().parent().next();
+
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({ opacity: 0 }, {
+            step: function (now) {
+                // for making fielset appear animation
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                next_fs.css({ 'opacity': opacity });
+            },
+            duration: 500
+        });
+
+        scrollToAnchor(".news__section", 300);
+    });
+
+});
+
 function readMore(target) {
     debugger
     var dots = document.getElementById("dots");
@@ -15,7 +69,7 @@ function readMore(target) {
     }
 }
 
-function scrollToAnchor(item) {
-	var item = $(item);
-	$('html,body').animate({ scrollTop: item.offset().top }, 'slow');
+function scrollToAnchor(item, speed) {
+    var item = $(item);
+    $('html,body').animate({ scrollTop: item.offset().top - 90 }, speed);
 } 
